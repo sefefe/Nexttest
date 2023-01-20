78d65110115c259b97ad9b16e8dbfff6276d41ca
sefefe <sefefe@gmial.com>
2023-01-20 07:40:44 +0300

    all

--- pages/docs/[...params].js
+++ pages/docs/[...params].js
@@ -1,5 +1,24 @@
 //Catch All routes
 
+import { useRouter } from "next/router";
+
+/*
+it is used to navigate like documentation sites with 
+many nested routs
+Feature1
+ -concept1
+- concept2
+- concept3
+- concept4
+Feature2
+ -concept1
+- concept2
+- concept3
+- concept4
+
+
+*/
 export default function  Docs(){
+    const router = useRouter()
     return <h1>docs</h1>;
 }
